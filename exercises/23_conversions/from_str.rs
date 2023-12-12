54d48ed08f18df14425f386832b7cc18f33388d9
ArchLance <lancee_zhang@163.com>
2023-12-12 21:17:20 +0800

    finish: all test finish
    Signed-off-by: ArchLance <lancee_zhang@163.com>

--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -12,6 +12,7 @@
 use std::num::ParseIntError;
 use std::str::FromStr;
 
+
 #[derive(Debug, PartialEq)]
 struct Person {
     name: String,
@@ -31,7 +32,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
 
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
@@ -52,6 +52,24 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+        let v: Vec<_> = s.split(",").collect();
+        if v.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        if v[0].is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        match v[1].parse::<usize>() {
+            Ok(age) =>Ok(Person{
+                name: v[0].to_string(),
+                age: age
+            }),
+            Err(e) => Err(ParsePersonError::ParseInt(e))
+        }
+        
     }
 }
 
